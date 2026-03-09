5f8b4c98f59fca98c062778d1c6c8d753b9c7b22
Ayuussshhh <149790144+Ayuussshhh@users.noreply.github.com>
2026-03-09 13:08:55 +0530

    changes G to g in datagrid

--- src/components/customComponent/index.js
+++ src/components/customComponent/index.js
@@ -4,7 +4,7 @@
  * Role-agnostic, reusable components.
  * Import: import { CustomDataGrid, CustomChip, KPICard } from 'src/components/customComponent'
  */
-export { default as CustomDataGrid } from './CustomDataGrid'
+export { default as CustomDataGrid } from './CustomDatagrid'
 export { default as CustomChip } from './CustomChip'
 export { default as KPICard } from './KPICard'
 export { default as AddDialog } from './AddDialog'
